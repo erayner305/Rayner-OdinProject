155e544a170052ec519758459c5e1ddaf9f85cda
OlaTheEast <easton.rayner@gmail.com>
2025-02-05 09:43:17 -0600

    added color switching support

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,9 +1,16 @@
 const root = document.documentElement;
 const container = document.querySelector("#container");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 let dimension = 16;
+let color = "#000000";
+const BACKGROUND_COLOR = "white";
 
 function buildGrid(dimension) {
 	container.innerHTML = "";
+	canvas.width = container.clientWidth;
+	canvas.height = container.clientHeight;
+
 	const gridItemDimension = 100 / dimension;
 	for (let i = 1; i <= dimension * dimension; i++) {
 		const gridItem = document.createElement("div");
@@ -26,7 +33,7 @@ updateButton.addEventListener("click", () => {
 });
 
 let resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", () => buildGrid(dimension))
+resetButton.addEventListener("click", () => buildGrid(dimension));
 
 let isLeftMouseDown = false;
 let isRightMouseDown = false;
@@ -35,11 +42,11 @@ container.addEventListener("mousedown", (event) => {
 	event.preventDefault();
 	switch (event.button) {
 		case 0:
-			event.target.style.backgroundColor = "black";
+			event.target.style.backgroundColor = color;
 			isLeftMouseDown = true;
 			break;
 		case 2:
-			event.target.style.backgroundColor = "gray";
+			event.target.style.backgroundColor = BACKGROUND_COLOR;
 			isRightMouseDown = true;
 			break;
 	}
@@ -53,13 +60,42 @@ document.addEventListener("mouseup", () => {
 container.addEventListener("mouseover", (event) => {
 	if (event.target.hasAttribute("class", "grid-item")) {
 		if (isLeftMouseDown) {
-			event.target.style.backgroundColor = "black";
+			event.target.style.backgroundColor = color;
 		} else if (isRightMouseDown) {
-			event.target.style.backgroundColor = "gray";
+			event.target.style.backgroundColor = BACKGROUND_COLOR;
 		}
 	}
 });
 
 container.addEventListener("contextmenu", (event) => {
 	event.preventDefault();
+});
+
+let colorPicker = document.querySelector("#color");
+colorPicker.addEventListener("input", (event) => {
+	color = event.target.value;
+});
+
+document.querySelector("#save").addEventListener("click", () => {
+	const gridItems = document.querySelectorAll(".grid-item");
+	const gridItemDimension = container.clientWidth / dimension;
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = BACKGROUND_COLOR;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	gridItems.forEach((item, index) => {
+		if (item.style.backgroundColor != BACKGROUND_COLOR) {
+			const x = (index % dimension) * gridItemDimension;
+			const y = Math.floor(index / dimension) * gridItemDimension;
+			console.log([x, y]);
+			ctx.fillStyle = item.style.backgroundColor || BACKGROUND_COLOR;
+			ctx.fillRect(x, y, gridItemDimension, gridItemDimension);
+		}
+	});
+
+	const link = document.createElement("a");
+	link.download = "canvas.jpg";
+	link.href = canvas.toDataURL("image/jpeg");
+	link.click();
 });
