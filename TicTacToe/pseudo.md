caeeb99282b3d3ea984673ee07f94444690f7e2d
Easton <easton.rayner@gmail.com>
2025-02-13 17:32:11 -0800

    Implemented most game logic

---
+++ TicTacToe/pseudo.md
@@ -0,0 +1,18 @@
+# Tic Tac Toe Rules
+
+## Overview
+- **Players**: 2 players participate.
+- **Objective**: Place your mark (X or O) to achieve three consecutive marks (horizontally, vertically, or diagonally).
+
+## How to Play
+1. Player 1 clicks a cell.
+2. Player 2 clicks a cell.
+3. Repeat until:
+   - A player achieves three in a row, or
+   - The board fills up, resulting in a draw.
+
+## Structures
+- **gameBoard**:
+- **cell**:
+- **player**:
+- **displayController**
---
+++ TicTacToe/script.js
@@ -0,0 +1,152 @@
+console.log("Use gameController.help() for some useful commands");
+
+/**
+ * gameController is responsible for monitoring the flow
+ * of the game
+ */
+
+let gameController = function (player1name, player2name) {
+	const MAX_ROUNDS = 9;
+	const PLAYER_1_SYMBOL = "X";
+	const PLAYER_2_SYMBOL = "O";
+	let currentPlayer, currentRound;
+
+	/**
+	 * player keeps track of each player and their score
+	 */
+	let player = function (symbol, name) {
+		let score = 0;
+
+		const getSymbol = () => symbol;
+		const getName = () => name;
+		const getScore = () => score;
+		const incrementScore = () => score++;
+
+		return { getSymbol, getName, getScore, incrementScore };
+	};
+
+	/**
+	 * gameBoard represents the state of the board
+	 * Each entry in the board array represents a cell
+	 * " " indicates an empty cell
+	 */
+
+	let gameBoard = (function () {
+		const blankBoard = [
+			[" ", " ", " "],
+			[" ", " ", " "],
+			[" ", " ", " "],
+		];
+
+		let board = [...blankBoard];
+
+		const getBoard = () => board;
+		const reset = () => {
+			board = [...blankBoard];
+		};
+		const replaceCell = (player, [cellRow, cellColumn]) => {
+			if (board[cellRow][cellColumn] == " ") {
+				board[cellRow][cellColumn] = player.getSymbol();
+				return true;
+			} else {
+				return false;
+			}
+		};
+		return { getBoard, reset, replaceCell };
+	})();
+
+	const playRound = () => {
+		console.log(`${currentPlayer.getName()}'s Turn!`);
+		return gameBoard.getBoard().forEach((row) => {
+			console.log(row);
+		});
+	};
+
+	const getCurrentPlayer = () => currentPlayer.getName();
+	const getcurrentRound = () => currentRound;
+
+	const chooseCell = (cellRow, cellColumn) => {
+		let success = gameBoard.replaceCell(currentPlayer, [cellRow, cellColumn]);
+		if (success) {
+			currentRound++;
+			if (currentRound <= 9) {
+				// No need to check before this since it's impossible
+				if (currentRound >= 6) {
+					if (checkWinCondition()) {
+						currentPlayer.incrementScore();
+						console.log(`${currentPlayer.getName()} Wins! Score is ${player1.getScore()} to ${player2.getScore()}`);
+						return startGame();
+					}
+				}
+				currentPlayer = currentRound % 2 == 0 ? player2 : player1;
+				return playRound();
+			} else {
+				return `Game Over! Tie!`;
+			}
+		} else {
+			return "That space is invalid! Try Again!";
+		}
+	};
+
+	const resetGame = () => {
+		gameBoard.reset();
+		player1 = player2 = currentPlayer = currentRound = undefined;
+		currentRound;
+	};
+
+	const checkWinCondition = () => {
+		const board = gameBoard.getBoard();
+		const winningCombinations = [
+			// Rows
+			[board[0][0], board[0][1], board[0][2]],
+			[board[1][0], board[1][1], board[1][2]],
+			[board[2][0], board[2][1], board[2][2]],
+			// Columns
+			[board[0][0], board[1][0], board[2][0]],
+			[board[0][1], board[1][1], board[2][1]],
+			[board[0][2], board[1][2], board[2][2]],
+			// Diagonals
+			[board[0][0], board[1][1], board[2][2]],
+			[board[0][2], board[1][1], board[2][0]],
+		];
+	
+		return winningCombinations.some(combination => {
+			return combination.every(cell => cell === "X") || combination.every(cell => cell === "O");
+		});
+	};
+
+	const help = () => {
+		console.log(
+			`use gameController.chooseCell(cellRow, cellColumn) to enter where you would like your symbol to go`
+		);
+		console.log(
+			`use gameController.resetGame() to clear the board and players`
+		);
+	};
+
+	let player1 = player(PLAYER_1_SYMBOL, player1name || "Player 1");
+	let player2 = player(PLAYER_2_SYMBOL, player2name || "Player 2");
+
+	const startGame = () => {
+		currentPlayer = player1;
+		currentRound = 1;
+		console.log(
+			`Game started between ${player1.getName()} (X) and ${player2.getName()} (O)`
+		);
+		return playRound();
+	};
+
+	startGame();
+
+	return {
+		getCurrentPlayer,
+		getcurrentRound,
+		chooseCell,
+		resetGame,
+		help,
+	};
+};
+
+let displayController = (function () {
+	return {};
+})();
