92e468f4a3382b72d8ac8418927c1b6ccfeebcd4
erayner <erayner@uda1.com>
2025-03-14 23:12:15 -0500

    Cleaned up some unused comments

--- TodoList/src/index.js
+++ TodoList/src/index.js
@@ -3,8 +3,6 @@ import "./style.css";
 import TodoManager from "./modules/TodoManager";
 import DOMManager from "./modules/DOMManager";
 
-// import exampleTodoList from "./testing/todoExamples";
-
 class App {
     constructor() {
         this.loadInitialData();
@@ -12,8 +10,6 @@ class App {
 
 	loadInitialData() {
         this.todoManager = new TodoManager();
-        // this.todoManager.addList(exampleTodoList[0]);
-        // this.todoManager.addList(exampleTodoList[1]);
         this.domManager = new DOMManager(this.todoManager);
     }
 
