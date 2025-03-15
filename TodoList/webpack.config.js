a058c893e2cdc286669224f5790422be62449eb2
erayner <erayner@uda1.com>
2025-03-14 23:17:36 -0500

    Added unfinished todo list project and update webpack config for it

--- TodoList/webpack.config.js
+++ TodoList/webpack.config.js
@@ -3,17 +3,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-    allowedHosts: "all",
   },
   plugins: [
     new HtmlWebpackPlugin({
