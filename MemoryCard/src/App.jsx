0dd8f6711f4525bb4769559cccfdf262e64ac913
Easton <easton.rayner@gmail.com>
2025-06-16 08:10:52 -0500

    Implemented images and shuffle logic for memory card

--- MemoryCard/src/App.jsx
+++ MemoryCard/src/App.jsx
@@ -2,46 +2,97 @@ import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
-const cards = [
+import frogdripJPG from "./assets/Frogdrip.jpg"
+import haybalerJPG from "./assets/Haybaler.jpg"
+import demonboneJPG from "./assets/Demonbone.jpg"
+import dirgeJPG from "./assets/Dirge.jpg"
+import dreadhowlJPG from "./assets/Dreadhowl.jpg"
+import jacynthJPG from "./assets/Jacynth.jpg"
+import januaryJPG from "./assets/January.jpg"
+import spectroJPG from "./assets/Spectro.jpg"
+import wizJPG from "./assets/Wiz.jpg"
+import zephirahJPG from "./assets/Zephirah.jpg"
+
+const initialCards = [
 	{
 		id: crypto.randomUUID(),
-		title: "Toot Toot",
-		img: "",
+		title: "Frogdrip",
+		img: frogdripJPG,
 	},
-    	{
+	{
 		id: crypto.randomUUID(),
-		title: "Toot Toot",
-		img: "",
+		title: "Haybaler",
+		img: haybalerJPG,
 	},
-    	{
+	{
 		id: crypto.randomUUID(),
-		title: "Toot Toot",
-		img: "",
+		title: "January",
+		img: januaryJPG,
 	},
-    	{
+	{
 		id: crypto.randomUUID(),
-		title: "Toot Toot",
-		img: "",
+		title: "Demonbone",
+		img: demonboneJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Zephirah",
+		img: zephirahJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Dirge",
+		img: dirgeJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Jacynth",
+		img: jacynthJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Dreadhowl",
+		img: dreadhowlJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Spectro",
+		img: spectroJPG,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "Wiz",
+		img: wizJPG,
 	},
 ];
 
-function resetClickedCards() {
+function shuffleArray(array) {
+	const arr = [...array];
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+}
+
+function resetClickedCards(cardsArr) {
 	const obj = {};
-	for (const card of cards) {
+	for (const card of cardsArr) {
 		obj[card.id] = false;
 	}
 	return obj;
 }
 
 function App() {
+	const [cards, setCards] = useState(initialCards);
 	const [currentScore, setCurrentScore] = useState(0);
 	const [highScore, setHighScore] = useState(0);
-	const [clickedCards, setClickedCards] = useState(resetClickedCards());
+	const [clickedCards, setClickedCards] = useState(resetClickedCards(initialCards));
 
 	function handleCardClick(id) {
 		if (clickedCards[id]) {
 			setCurrentScore(0);
-			setClickedCards(resetClickedCards());
+			setClickedCards(resetClickedCards(cards));
 		} else {
 			setClickedCards((prev) => {
 				return { ...prev, [id]: true };
@@ -50,6 +101,8 @@ function App() {
 			setCurrentScore(updatedScore);
 			if (updatedScore > highScore) setHighScore(updatedScore);
 		}
+		// Shuffle cards after every click
+		setCards((prevCards) => shuffleArray(prevCards));
 	}
 
 	return (
--- MemoryCard/src/components/Card.jsx
+++ MemoryCard/src/components/Card.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function Card({ id, title, img, handleCardClick }) {
 
+	const IMG_WIDTH = "320px";
+	const IMG_HEIGHT = IMG_WIDTH;
+
 	return (
 		<div
 			className="card"
@@ -9,8 +12,8 @@ export default function Card({ id, title, img, handleCardClick }) {
 				handleCardClick(id);
 			}}
 		>
+			<img src={img} alt={title} width={IMG_WIDTH} height={IMG_HEIGHT} />
 			<div className="title">{title}</div>
-			<img src={img} alt={title} />
 		</div>
 	);
 }
